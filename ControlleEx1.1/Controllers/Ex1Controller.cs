c55ccc73da55bbb36a0efef9213a0d860a6260b6
Raj Mitra <indiahind320@gmail.com>
2022-07-06 17:02:35 +0530

    Controller App Files

---
+++ ControlleEx1.1/Controllers/Ex1Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlleEx1._1.Controllers
+{
+    public class Ex1Controller : Controller
+    {
+        List<String> breakingNews = new List<String>()      //DO NOT change this declaration and values
+        {
+            "PM visit brings business","10% slash in GST","Top Player announced retirement","India wins series"
+        };
+        // GET: Ex1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AllNews()
+        {
+            string temp = "";
+            foreach(var q in breakingNews)
+            {
+                temp = temp + q + "</br>";
+            }
+            
+            return Content(temp);
+        }
+
+        public ActionResult NewsByChoice(int id)
+        {
+            int count = 1;
+            string temp = "";
+            foreach(var q in breakingNews)
+            {
+                temp = q;
+                if(count == id)
+                {
+                    break;
+                }
+                count++;
+            }
+            return Content(temp);
+        }
+    }
+}
---
+++ ControlleEx1.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControlleEx1._1.MvcApplication" Language="C#" %>
